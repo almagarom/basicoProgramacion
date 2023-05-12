7884443f6531d1ae7d74105648e43ffe9c34860b
AlmaGarcia <alma1im19@gmail.com>
2023-05-12 13:24:47 -0600

    Complete JavaScript file

--- js/main.js
+++ js/main.js
@@ -6,7 +6,16 @@ function seleccionarPokemon(){
 let pokemon = ""
 let ataqueJugador = ""
 let mostrarAtaqueJugador
+let AtaqueEnemigo
+let combateFinal
+let vidasJugadorPokemon = 3
+let vidasEnemigoPokemon = 3
 function iniciarJuego(){
+    //Para ocultar los ataques al inicio
+    document.getElementById("selectAtack").style.display = "none"
+    document.getElementById("restart").style.display= "none"
+
+
     //Escuchar cuando se le de click al boton de seleccionar
     //document, para que revise el documento HTML
     let botonSeleccionarPokemon = document.getElementById("select")
@@ -18,6 +27,7 @@ function iniciarJuego(){
     document.getElementById("fire").addEventListener("click", ataqueFuego)
     document.getElementById("water").addEventListener("click", ataqueAgua)
     document.getElementById("ground").addEventListener("click", ataqueTierra)
+    document.getElementById("restartear").addEventListener("click", reiniciarJuego)
     mostrarAtaqueJugador = document.getElementById("ataqueJugador")
 }
 
@@ -26,6 +36,8 @@ function iniciarJuego(){
 window.addEventListener("load", iniciarJuego )
 
 function elegirPokemon (){
+    document.getElementById("selectMokepon").style.display = "none"
+    document.getElementById("selectAtack").style.display = "block"
     //Agrego para todos los INPUTS para detectar si han sido seleccionados o no
     let pikachu = document.getElementById("pikachu")
     let rhydon  = document.getElementById("rhydon")
@@ -53,6 +65,7 @@ function elegirPokemon (){
         alert("Selecciona un Pokemon")
     }
     enemigoAleatorio(1,3)
+
 }
 
 function enemigoAleatorio(min,max){
@@ -72,13 +85,15 @@ function ataqueEnemigoAleatorio(min,max){
     let ataqueSeleccionadoEnemigo = document.getElementById("ataqueEnemigo")
     let aleatorio = Math.floor(Math.random()*(max-min+1)+min)
     if(aleatorio==1){
-        
+        AtaqueEnemigo = "Fuego"
         console.log("Fuego enemigo")
         //ataqueSeleccionadoEnemigo.innerHTML= "Fuego"
     } else if(aleatorio==2){
+        AtaqueEnemigo = "Agua"
         console.log("Agua enemigo")
        // ataqueSeleccionadoEnemigo.innerHTML= "Agua"
     }else if(aleatorio==3){
+        AtaqueEnemigo = "Tierra"
         console.log("tierra enemigo")
         //ataqueSeleccionadoEnemigo.innerHTML= "Tierra"
     }
@@ -88,6 +103,7 @@ function ataqueFuego(){
     ataqueJugador= "Fuego"
     console.log(ataqueJugador)
     ataqueEnemigoAleatorio(1,3)
+    combate()
     //mostrarAtaqueJugador.innerHTML = ataqueJugador
 }
 
@@ -95,6 +111,7 @@ function ataqueAgua(){
     ataqueJugador= "Agua"
     console.log(ataqueJugador)
     ataqueEnemigoAleatorio(1,3)
+    combate()
     //mostrarAtaqueJugador.innerHTML = ataqueJugador
 }
 
@@ -102,5 +119,81 @@ function ataqueTierra(){
     ataqueJugador= "Tierra"
     console.log(ataqueJugador)
     ataqueEnemigoAleatorio(1,3)
+    combate()
     //mostrarAtaqueJugador.innerHTML = ataqueJugador
+}
+
+//agrego nueva funcion para enviar los ataques que está haciendo cada uno, el jugador y el enemigo
+function createMessage(final){
+    //para que sepa donde se va a mostrar
+    var seccionAtaques = document.getElementById("mensajes")
+
+
+    //entre comillas le decimos cual es el tipo de etiqueta HTML que queremos mostrar
+    var parrafo = document.createElement("p")
+    parrafo.innerHTML = "Tu mascota atacó con"+ ataqueJugador+" el enemigo atacó con "+ AtaqueEnemigo + " " + final
+    seccionAtaques.appendChild(parrafo)
+}
+
+//Para saber si ganamos o perdimos
+function combate (){
+    vidasEnemigoPokemonModificar= document.getElementById("vidasEnemigo")
+    vidasJugadorPokemonModificar= document.getElementById("vidasJugador")
+    if(ataqueJugador == AtaqueEnemigo){
+        combateFinal= "EMPATE"
+    } else if(ataqueJugador== "Fuego" && AtaqueEnemigo== "Tierra"){
+        combateFinal = "GANASTE"
+        vidasEnemigoPokemon -= 1
+    }else if(ataqueJugador== "Agua" && AtaqueEnemigo== "Fuego"){
+        combateFinal = "GANASTE"
+        vidasEnemigoPokemon -= 1
+    }else if(ataqueJugador== "Tierra" && AtaqueEnemigo== "Agua"){
+        combateFinal = "GANASTE"
+        vidasEnemigoPokemon -= 1
+    }
+    else{
+        combateFinal = "PERDISTE"
+        vidasJugadorPokemon -= 1
+    }
+    console.log(combateFinal)
+    createMessage(combateFinal)
+    vidasEnemigoPokemonModificar.innerHTML = vidasEnemigoPokemon
+    vidasJugadorPokemonModificar.innerHTML = vidasJugadorPokemon
+
+    revisarVidas()
+}
+
+function revisarVidas (){
+    if(vidasEnemigoPokemon == 0){
+        alert("GANASTE!!!!")
+        createMessageFinal("Felicitaciones! Ganaste!!")
+    }else if(vidasJugadorPokemon == 0){
+        alert("PERDISTE!!!")
+        createMessageFinal("Lástima! Perdiste!! 😔")
+    }
+}
+
+
+//Para enviar el mensaje del resultado final
+function createMessageFinal(mensajeFinal){
+    //para que sepa donde se va a mostrar
+    var seccionAtaques = document.getElementById("mensajes")
+
+
+    //entre comillas le decimos cual es el tipo de etiqueta HTML que queremos mostrar
+    var parrafo = document.createElement("p")
+    parrafo.innerHTML = mensajeFinal
+    seccionAtaques.appendChild(parrafo)
+    //Para deshabilitar los botones de los ataques cuando ya no se tengan vidas
+    document.getElementById("fire").disabled = true
+    document.getElementById("water").disabled = true
+    document.getElementById("ground").disabled = true
+
+    document.getElementById("restart").style.display= "block"
+}
+
+function reiniciarJuego(){
+    //location.reload: se refiere a la ubicación
+    //Funciona para recargar la página
+    location.reload()
 }
